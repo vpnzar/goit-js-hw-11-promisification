4ee3275f1c3e3814e676c562c599c8b1cc4a061a
Vladymyr Nazarenko <vpnazarenko@gmail.com>
2021-08-15 16:12:29 +0300

    Summary

---
+++ src/js/task-03.js
@@ -0,0 +1,37 @@
+// ## Задание 3 >>>>>>>>>>>>>>>>>>>>>>>>
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = transaction => {
+  const delay = randomIntegerFromInterval(200, 500);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+
+      if (canProcess) {
+        resolve([transaction.id, delay]);
+      } else {
+        reject(transaction.id);
+      }
+    }, delay);
+  });
+};
+
+const logSuccess = id => {
+  console.log(`Transaction ${id[0]} processed in ${id[1]}ms`);
+};
+
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
